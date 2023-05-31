2c2b6f52362bd8a12283644b41452167e4bf977a
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-05-30 23:31:53 -0300

    posts start of css and logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import TimelinePage from "./pages/TimelinePage/index.js";
 import TrendingPage from "./pages/TrendingPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -6,6 +7,7 @@ export default function App() {
     <Router className="App">
       <Routes>
         <Route path="/" element={<TrendingPage />} />
+        <Route path="/timeline" element={<TimelinePage />} />
       </Routes>
     </Router>
   );
