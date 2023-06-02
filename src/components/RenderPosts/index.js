58a82dd6ccdf5334f896f21cc3d055ddcf660ade
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-06-02 19:43:59 -0300

    feat/login

--- src/components/RenderPosts/index.js
+++ src/components/RenderPosts/index.js
@@ -1,17 +1,61 @@
 import { useEffect, useState } from "react";
 import { DescriptionContainer, PostContainer, PostContentContainer, UrlContainer, UserName } from "../../pages/TimelinePage/styles.js";
 import LinkPreview from "../LinkPreview/index.jsx";
-import { ProfilePicture, ProfilePictureContainer } from "../PublishPost/index.js";
-import { StyledTrash } from "./styles.js";
+import { FilledHeart, HeartOutline, LikeContainer, ProfilePicture, ProfilePictureContainer, StyledTrash } from "./styles.js";
 import DialogBox from "../Dialog/index.js";
 import { UserContext } from "../../App.js";
 import { useContext } from "react";
+import axios from "axios";
 
 
 export function RenderPosts(props) {
-    const { picture_url, username, description, url, id, setReload, user_id } = props;
+    const { picture_url, username, description, url, id, setReload, user_id, user_liked, total_likes, liked_users, reloadPage, tokenJson, token } = props;
     const [showModal, setShowModal] = useState(false)
     const { userInfo, setUserInfo } = useContext(UserContext)
+    const [isLiked, setIsLiked] = useState(user_liked)
+    const [isDisabled, setIsDisabled] = useState(false);
+    const axiosUrl = `${process.env.REACT_APP_API_URL}/like`
+
+    function likePost() {
+        const config = {
+            headers: { authorization: `Bearer ${token.token}` }
+        };
+        const body = {
+            user_id: tokenJson.id,
+            post_id: id
+        }
+        setIsDisabled(true)
+        if (isLiked) {
+            const promise = axios.delete(axiosUrl, {
+                headers: { authorization: `Bearer ${token.token}` },
+                data: body
+            });
+            promise
+                .then(() => {
+                    setIsDisabled(false)
+                    setIsLiked(false)
+                    setReload(!reloadPage)
+                })
+                .catch((a) => {
+                    console.log("erro", a)
+                    setIsDisabled(false)
+                });
+
+        }
+        else {
+            const promise = axios.post(axiosUrl, body, config)
+            promise
+                .then(() => {
+                    setIsDisabled(false)
+                    setIsLiked(true)
+                    setReload(!reloadPage)
+                })
+                .catch((a) => {
+                    console.log("erro", a.response.data)
+                    setIsDisabled(false)
+                });
+        }
+    }
 
     return (
         <>
@@ -22,6 +66,11 @@ export function RenderPosts(props) {
                         onError={(e) => {
                             e.target.src = 'https://cdn.onlinewebfonts.com/svg/img_258083.png';
                         }}></ProfilePicture>
+                    <LikeContainer>
+                        <button disabled={isDisabled} onClick={likePost}>{isLiked ? <FilledHeart></FilledHeart>
+                            : <HeartOutline></HeartOutline>}</button>
+                        <p>{total_likes} {total_likes == 1 ? 'like' : 'likes'}</p>
+                    </LikeContainer>
                 </ProfilePictureContainer>
                 <PostContentContainer>
                     <UserName>{username}</UserName>
@@ -29,7 +78,7 @@ export function RenderPosts(props) {
                     <UrlContainer>
                         <LinkPreview url={url}></LinkPreview>
                     </UrlContainer>
-                    { (userInfo.id) == (user_id) && <StyledTrash onClick={() => setShowModal(true)} />}
+                    {(userInfo.id) == (user_id) && <StyledTrash onClick={() => setShowModal(true)} />}
                 </PostContentContainer>
             </PostContainer>
             <DialogBox
@@ -41,3 +90,4 @@ export function RenderPosts(props) {
         </>
     );
 }
+
--- src/components/RenderPosts/styles.js
+++ src/components/RenderPosts/styles.js
@@ -1,5 +1,6 @@
 import { FaTrash } from 'react-icons/fa';
 import styled from "styled-components";
+import { IoHeartOutline, IoHeart } from "react-icons/io5";
 
 export const StyledTrash = styled(FaTrash)`
     color: white;
@@ -11,4 +12,55 @@ export const StyledTrash = styled(FaTrash)`
         cursor: pointer;
         transform: scale(1.2);
     }
+`
+
+export const LikeContainer = styled.div`
+    min-width: 50px;
+    min-height: 50px;
+    position: absolute;
+    top: 69px;
+    left: 17px;
+    display:flex;
+    align-items: center;
+    flex-direction: column;
+    p{
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 11px;
+        line-height: 13px;
+        text-align: center;
+        margin-top: 2px;
+        color: #FFFFFF;
+    }
+    button{
+        all: unset;
+    }
+`
+
+export const ProfilePicture = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 26.5px;
+    background-color: blue;
+    background-image: url(${props => props.pictureUrl});
+    background-size: cover;
+    background-position: center;
+    position: absolute;
+    left: 17px;
+`;
+export const ProfilePictureContainer = styled.div`
+    width: 86px;
+    height: 209px;
+    display:flex;
+    position: relative;
+`
+
+export const HeartOutline = styled(IoHeartOutline)`
+    color: white;
+    font-size: 25px;
+`
+export const FilledHeart = styled(IoHeart)`
+    color: red;
+    font-size: 25px;
 `
