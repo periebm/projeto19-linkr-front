da315fa4a159e7b12656e9ac90f5e1d05ceb5aa7
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-06-08 17:23:51 -0300

    fix: hub issues

--- src/components/RenderPosts/index.js
+++ src/components/RenderPosts/index.js
@@ -243,6 +243,7 @@ export function RenderPosts({
                     <UserName data-test="username" onClick={() => navigate(`/user/${user_id}`)}>{username}</UserName>
                     {isEditing ? (
                         <EditInput
+                            data-test="edit-input"
                             type="text"
                             ref={inputRef}
                             value={descriptionState}
@@ -258,7 +259,7 @@ export function RenderPosts({
                     <UrlContainer>
                         <LinkPreview url={url}></LinkPreview>
                     </UrlContainer>
-                    {(userInfo.id) === (user_id) && <StyledPencil onClick={handleEditClick} />}
+                    {(userInfo.id) === (user_id) && <StyledPencil data-test="edit-btn" onClick={handleEditClick} />}
                     {(userInfo.id) === (user_id) && <StyledTrash data-test="delete-btn" onClick={() => setShowModal(true)} />}
                 </PostContentContainer>
             </PostContainer>
